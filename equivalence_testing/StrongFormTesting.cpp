f35028498c3ee4d9dd68250bc1295b3b179950aa
Chychen <jaycase.cs01@gmail.com>
2017-04-04 21:48:53 +0800

    finish strong form testing of equivalence testing

--- equivalence_testing/StrongFormTesting.cpp
+++ equivalence_testing/StrongFormTesting.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../feature_code/Link_List.h"
 
-class LinkListTest : public ::testing::Test {
+class Strong_LinkListTest : public ::testing::Test {
     protected:
         virtual void SetUp() {
             linkList.insert_node(10);
@@ -17,6 +17,44 @@ class LinkListTest : public ::testing::Test {
 };
 
 // Strong Form
-TEST_F(LinkListTest, StrongFormTesting){
+TEST_F(Strong_LinkListTest, OperatorSquareBrackets){
+    // Weak Noraml
     EXPECT_EQ(linkList[0], 10);
+    // Weak Robust
+    // EXPECT_EQ(linkList[-1], 0); no ideas how to test
+    // Strong Noraml
+    // Strong Robust
+}
+
+TEST_F(Strong_LinkListTest, InsertNode){
+    // Weak Noraml
+    EXPECT_EQ(linkList.insert_node(15), true);
+    // Weak Robust
+    // EXPECT_EQ(linkList.insert_node(), true);
+    // Strong Noraml
+        // no multi falut
+    // Strong Robust
+        // no multi falut
+}
+
+TEST_F(Strong_LinkListTest, InsertIndexNode){
+    // Weak Noraml
+    EXPECT_EQ(linkList.insert_node(5, 15), true);
+    // Weak Robust
+    EXPECT_EQ(linkList.insert_node(-1, 16), false);
+    // Strong Noraml
+    EXPECT_EQ(linkList.insert_node(1, 17), true);
+    // Strong Robust
+    EXPECT_EQ(linkList.insert_node(-1, -1), false);
+}
+
+TEST_F(Strong_LinkListTest, DeleteIndexNode){
+    // Weak Noraml
+    EXPECT_EQ(linkList.delete_node(5), true);
+    // Weak Robust
+    EXPECT_EQ(linkList.delete_node(-1), false);
+    // Strong Noraml
+        // no multi falut
+    // Strong Robust
+        // no multi falut
 }
--- equivalence_testing/WeakFormTesting.cpp
+++ equivalence_testing/WeakFormTesting.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../feature_code/Link_List.h"
 
-class LinkListTest : public ::testing::Test {
+class WeakFormLinkListTest : public ::testing::Test {
     protected:
         virtual void SetUp() {
             linkList.insert_node(10);
@@ -16,28 +16,28 @@ class LinkListTest : public ::testing::Test {
         Link_List linkList;
 };
 
-TEST_F(LinkListTest, OperatorSquareBrackets){
+TEST_F(WeakFormLinkListTest, OperatorSquareBrackets){
     // Weak Noraml
     EXPECT_EQ(linkList[0], 10);
     // Weak Robust
     // EXPECT_EQ(linkList[-1], 0); no ideas how to test
 }
 
-TEST_F(LinkListTest, InsertNode){
+TEST_F(WeakFormLinkListTest, InsertNode){
     // Weak Noraml
     EXPECT_EQ(linkList.insert_node(15), true);
     // Weak Robust
     // EXPECT_EQ(linkList.insert_node(), true);
 }
 
-TEST_F(LinkListTest, InsertIndexNode){
+TEST_F(WeakFormLinkListTest, InsertIndexNode){
     // Weak Noraml
     EXPECT_EQ(linkList.insert_node(5, 15), true);
     // Weak Robust
     EXPECT_EQ(linkList.insert_node(-1, 16), false);
 }
 
-TEST_F(LinkListTest, DeleteIndexNode){
+TEST_F(WeakFormLinkListTest, DeleteIndexNode){
     // Weak Noraml
     EXPECT_EQ(linkList.delete_node(5), true);
     // Weak Robust
